fb451adc0432404bf97a616754c58881958c7dbe
sanojsubran <sanoj.subran@ibm.com>
2026-06-15 17:02:12 +0200

    ci: install go manually
    Signed-off-by: sanojsubran <sanoj.subran@ibm.com>

--- .sonar.setup.bash
+++ .sonar.setup.bash
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-apt-get -y install golang make
+apt-get -y install make curl
+
+GO_VERSION=1.26.0
+GO_ARCH=amd64
+
+curl -LO https://go.dev/dl/go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
+rm -rf /usr/local/go && \
+tar -C /usr/local -xzf go${GO_VERSION}.linux-${GO_ARCH}.tar.gz && \
+rm -f go${GO_VERSION}.linux-${GO_ARCH}.tar.gz
+
+export GOROOT="/usr/local/go"
+export GOPATH="/root/go"
+export PATH="/usr/local/go/bin:/root/go/bin:${PATH}"
 
 make test-coverage
